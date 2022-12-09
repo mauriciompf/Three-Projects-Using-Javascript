d448d8a8984027c055bfade4b255505a430fc298
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2022-12-09 10:31:12 -0400

    update index.js

--- 1-passenger-counter-app/index.js
+++ 1-passenger-counter-app/index.js
@@ -1,1 +1,9 @@
-document.getElementById("count-el").innerText = 5;
+// document.getElementById("count-el").innerText = 5;
+
+// let firstBatch = 5;
+
+// let secondBatch = 8;
+
+// let count = firstBatch + secondBatch;
+
+// console.log(count);
