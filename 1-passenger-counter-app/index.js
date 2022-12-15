32b90628bb62df917e1d2a8ffcd8c891acded7a3
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2022-12-15 11:13:06 -0400

    fix: delete some comments

--- 1-passenger-counter-app/index.js
+++ 1-passenger-counter-app/index.js
@@ -1,84 +1,3 @@
-
-// document.getElementById("count-el").innerText = 5;
-
-// let firstBatch = 5;
-
-// let secondBatch = 8;
-
-// let count = firstBatch + secondBatch;
-
-// console.log(count);
-
-
-// let myAge = 20;
-
-// let humanDogRatio = 7;
-
-// let myDogAge = myAge * humanDogRatio
-
-// console.log(myDogAge);
-
-
-// count + 1
-// let count = 2
-
-// count = count * 9
-
-// console.log(count)
-
-// let bonusPoints = 50;
-// console.log(bonusPoints)
-
-// bonusPoints = bonusPoints * 2
-// console.log(bonusPoints)
-
-// bonusPoints = bonusPoints / 4
-// console.log(bonusPoints)
-
-// bonusPoints = bonusPoints * 3
-// console.log(bonusPoints)
-
-
-// function numberLog() {
-// 	console.log(42)
-// }
-
-// numberLog()
-
-
-// let lap1 = 34
-// let lap2 = 33
-// let lap3 = 36
-
-// function lapSum() {
-// 	console.log(lap1 + lap2 + lap3) 
-// }
-// lapSum()
-
-
-// let lapsCompleted = 0
-
-// function countLaps() {
-// 	lapsCompleted = lapsCompleted + 1
-// }
-// countLaps()
-// countLaps()
-// countLaps()
-// console.log(lapsCompleted)
-
-// let username = "per"
-// let message = "You have three now notifications"
-// console.log(message + ", " + username + "!")
-
-// let messageToUser = message + ", " + username + "!"
-// console.log(messageToUser)
-
-
-// let name = "Maurício"
-// let greeting = "Hi, my name is "
-// let myGreeting = greeting + name
-// console.log(myGreeting) 
-
 let count = 0
 
 function increment() {
@@ -87,7 +6,6 @@ function increment() {
 	count = count + 1
 
 	countEl.innerHTML = count
-
 }
 
 function save() {
@@ -99,8 +17,6 @@ function save() {
 // ====================
 // test _1.html
 // ====================
-/*global console , $ , document*/
-/*eslint-disable no-console*/
 
 let welcomeEl = document.getElementById("welcome-el")
 let name = "Maurício"
