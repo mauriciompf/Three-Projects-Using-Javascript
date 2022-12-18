a32240fffa13c5886cb00915a5a6fd635f718b84
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2022-12-18 10:06:22 -0400

    script.js update

--- 2-blackjack-game/script.js
+++ 2-blackjack-game/script.js
@@ -1,22 +1,27 @@
 // Blackjack logic win and lose
-
-let fcard = 10
-let scard = 16
-
+let fcard = 11
+let scard = 10
 let sum = fcard + scard
+let hasBackJack = false
+let isAlive = true
 
+let message = " "
 
-if (sum < 21) {
-	console.log("Do you want to draw a new card?")
-} else if (sum === 21) {
-	console.log("You've got Blackjack!")
-} else {
-	console.log("You're out of the game!")
+function startGame() {
+	if (sum <= 20) {
+		message = "Do you want to draw a new card?"
+	} else if (sum === 21) {
+		message = "Wohoo!, You've got Blackjack!"
+		hasBackJack = true
+	} else {
+		message = "You are out of the game!"
+		isAlive = false
+	}
+	console.log(message)
 }
 
-// Old enough to enter the club
-
-let age = 20
+// Old enough for enter the club
+let age = 22
 
 let ageEnter = 21
 
@@ -24,4 +29,13 @@ if (age >= ageEnter) {
 	console.log("Welcome!")
 } else {
 	console.log("You can not enter the club")
+}
+
+// Old enough for a birthday card from the King (100)
+if (age < 100) {
+	console.log("Not a elegible")
+} else if (age === 100) {
+	console.log("Here is your birthday card from the King!")
+} else {
+	console.log("Not elegible, you have already gotten one")
 }
