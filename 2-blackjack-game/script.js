7acee480cc2734985a1040695d6e947a88060ea5
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2022-12-19 11:36:32 -0400

    script.js update

--- 2-blackjack-game/script.js
+++ 2-blackjack-game/script.js
@@ -1,13 +1,22 @@
 // Blackjack logic win and lose
-let fcard = 11
-let scard = 10
+let fcard = 10
+let scard = 4
+let cards = [fcard, scard]
 let sum = fcard + scard
 let hasBackJack = false
 let isAlive = true
-
+let messageEl = document.querySelector("#message-el")
+let cardEl = document.querySelector(".card-el")
+let sumEl = document.querySelector(".sum-el")
 let message = " "
 
 function startGame() {
+	renderGame()
+}
+
+function renderGame() {
+	cardEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+ 	sumEl.textContent = "Sum: " + sum
 	if (sum <= 20) {
 		message = "Do you want to draw a new card?"
 	} else if (sum === 21) {
@@ -17,7 +26,13 @@ function startGame() {
 		message = "You are out of the game!"
 		isAlive = false
 	}
-	console.log(message)
+	messageEl.textContent = message
+}
+
+function newCard() {
+	let card = 11
+	sum += card
+	startGame()
 }
 
 // Old enough for enter the club
