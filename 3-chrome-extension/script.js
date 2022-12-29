3b4496779e28fc553ceae4683f569667e8fc5f72
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2022-12-29 18:43:22 -0400

    update script.js

---
+++ 3-chrome-extension/script.js
@@ -0,0 +1,19 @@
+let myLeads = ['asd', '12121', '2sda'];
+const saveBtn = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el");
+const ulEl = document.querySelector("#list-leads");
+
+saveBtn.addEventListener("click", function saveLead() {
+	myLeads.push(inputEl.value);
+}) 
+
+let listItems = ""
+
+for (let i = 0; i < myLeads.length; i++) {
+	listItems += "<li>" + myLeads[i] + "</li>"
+	// const li = document.createElement("li")
+	// li.textContent = myLeads[i]
+	// ulEl.append(li)
+}
+
+ulEl.innerHTML = listItems;
