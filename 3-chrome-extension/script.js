7cb5315ad158e77baeba4d3df50cda3a80ee4d7f
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2022-12-31 19:15:06 -0400

    update script.js

--- 3-chrome-extension/script.js
+++ 3-chrome-extension/script.js
@@ -1,6 +1,7 @@
 let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const saveBtn = document.querySelector("#input-btn");
+const tabBtn = document.querySelector("#tab-btn");
 const deleteBtn = document.querySelector("#delete-btn");
 const ulEl = document.querySelector("#nav-list");
 
@@ -8,34 +9,42 @@ const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
 
 if (leadsFromLocalStorage) {
 	myLeads = leadsFromLocalStorage;
-	renderLeads();
-}
+	render(myLeads);
+};
 
 saveBtn.addEventListener("click", function saveLead() {
 	myLeads.push(inputEl.value);
 	inputEl.value = "";
 	localStorage.setItem("myLeads", JSON.stringify(myLeads));
-	renderLeads();
-})
+	render(myLeads);
+});
+
+const tabs = [
+		{url: "https://www.youtube.com/watch?v=FzqL8iYkbKo"}
+	]
+
+tabBtn.addEventListener("click", function tab() {
+	console.log(tabs[0]);
+});
 
 deleteBtn.addEventListener("dblclick", function deleteLead() {
 	localStorage.clear();
 	myLeads = [];
-	renderLeads();
-})
+	render(myLeads);
+});
 
-function renderLeads() {
+function render(leads) {
 	let listItems = "";
 
-	for (let i = 0; i < myLeads.length; i++) {
+	for (let i = 0; i < leads.length; i++) {
 		listItems += `
 			<li class='item-list'>
-				<a class='link' target='_blank' href='${myLeads[i]}'>
-					${myLeads[i]}
+				<a class='link' target='_blank' href='${leads[i]}'>
+					${leads[i]}
 				</a>
 			</li>
 		`;
 	}
 
 	ulEl.innerHTML = listItems;
-}
+};
