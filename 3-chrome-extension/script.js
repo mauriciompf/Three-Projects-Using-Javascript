928cb6bc45eac46622cc6c0a9296da7e460f7f5e
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2022-12-31 16:55:58 -0400

    update script.js

--- 3-chrome-extension/script.js
+++ 3-chrome-extension/script.js
@@ -1,16 +1,31 @@
 let myLeads = [];
-const saveBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
+const saveBtn = document.querySelector("#input-btn");
+const deleteBtn = document.querySelector("#delete-btn");
 const ulEl = document.querySelector("#nav-list");
+
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
+
+if (leadsFromLocalStorage) {
+	myLeads = leadsFromLocalStorage;
+	renderLeads();
+}
 
 saveBtn.addEventListener("click", function saveLead() {
 	myLeads.push(inputEl.value);
+	inputEl.value = "";
+	localStorage.setItem("myLeads", JSON.stringify(myLeads));
 	renderLeads();
-	inputEl.value = ""
-}) 
+})
+
+deleteBtn.addEventListener("dblclick", function deleteLead() {
+	localStorage.clear();
+	myLeads = [];
+	renderLeads();
+})
 
 function renderLeads() {
-	let listItems = ""
+	let listItems = "";
 
 	for (let i = 0; i < myLeads.length; i++) {
 		listItems += `
@@ -19,10 +34,7 @@ function renderLeads() {
 					${myLeads[i]}
 				</a>
 			</li>
-		`
-			// "<li>" + 
-			// 	"<a href=" + myLeads[i] + " target='_blank'>" + myLeads[i] + "</a>"+
-			// "</li>"
+		`;
 	}
 
 	ulEl.innerHTML = listItems;
