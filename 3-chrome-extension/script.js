6eba3ab2b59f805450bd1de2bf7ceb48f5dd3d8a
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2022-12-31 10:18:14 -0400

    update script.js

--- 3-chrome-extension/script.js
+++ 3-chrome-extension/script.js
@@ -1,19 +1,29 @@
-let myLeads = ['asd', '12121', '2sda'];
+let myLeads = [];
 const saveBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
-const ulEl = document.querySelector("#list-leads");
+const ulEl = document.querySelector("#nav-list");
 
 saveBtn.addEventListener("click", function saveLead() {
 	myLeads.push(inputEl.value);
+	renderLeads();
+	inputEl.value = ""
 }) 
 
-let listItems = ""
+function renderLeads() {
+	let listItems = ""
 
-for (let i = 0; i < myLeads.length; i++) {
-	listItems += "<li>" + myLeads[i] + "</li>"
-	// const li = document.createElement("li")
-	// li.textContent = myLeads[i]
-	// ulEl.append(li)
+	for (let i = 0; i < myLeads.length; i++) {
+		listItems += `
+			<li class='item-list'>
+				<a class='link' target='_blank' href='${myLeads[i]}'>
+					${myLeads[i]}
+				</a>
+			</li>
+		`
+			// "<li>" + 
+			// 	"<a href=" + myLeads[i] + " target='_blank'>" + myLeads[i] + "</a>"+
+			// "</li>"
+	}
+
+	ulEl.innerHTML = listItems;
 }
-
-ulEl.innerHTML = listItems;
