cd796b8e4661d0d51716fda7697d5a37b00f123b
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-01-01 15:45:30 -0400

    update script.js

--- 3-chrome-extension/script.js
+++ 3-chrome-extension/script.js
@@ -1,40 +1,40 @@
-let myLeads = [];
-const inputEl = document.querySelector("#input-el");
-const saveBtn = document.querySelector("#input-btn");
-const tabBtn = document.querySelector("#tab-btn");
-const deleteBtn = document.querySelector("#delete-btn");
-const ulEl = document.querySelector("#nav-list");
+let myLeads = []
+const inputEl = document.querySelector("#input-el")
+const saveBtn = document.querySelector("#input-btn")
+const tabBtn = document.querySelector("#tab-btn")
+const deleteBtn = document.querySelector("#delete-btn")
+const ulEl = document.querySelector("#nav-list")
 
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
-	myLeads = leadsFromLocalStorage;
-	render(myLeads);
-};
+	myLeads = leadsFromLocalStorage
+	render(myLeads)
+}
 
 saveBtn.addEventListener("click", function saveLead() {
-	myLeads.push(inputEl.value);
-	inputEl.value = "";
-	localStorage.setItem("myLeads", JSON.stringify(myLeads));
-	render(myLeads);
-});
+	myLeads.push(inputEl.value)
+	inputEl.value = ""
+	localStorage.setItem("myLeads", JSON.stringify(myLeads))
+	render(myLeads)
+})
 
-const tabs = [
-		{url: "https://www.youtube.com/watch?v=FzqL8iYkbKo"}
-	]
-
-tabBtn.addEventListener("click", function tab() {
-	console.log(tabs[0]);
-});
+tabBtn.addEventListener("click", function saveTab() {
+	chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+		myLeads.push(tabs[0].url)
+		localStorage.setItem("myLeads", JSON.stringify(myLeads))
+		render(myLeads)
+	})
+})
 
 deleteBtn.addEventListener("dblclick", function deleteLead() {
-	localStorage.clear();
-	myLeads = [];
-	render(myLeads);
-});
+	localStorage.clear()
+	myLeads = []
+	render(myLeads)
+})
 
 function render(leads) {
-	let listItems = "";
+	let listItems = ""
 
 	for (let i = 0; i < leads.length; i++) {
 		listItems += `
@@ -43,8 +43,8 @@ function render(leads) {
 					${leads[i]}
 				</a>
 			</li>
-		`;
+		`
 	}
 
-	ulEl.innerHTML = listItems;
-};
+	ulEl.innerHTML = listItems
+}
